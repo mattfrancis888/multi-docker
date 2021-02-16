8c63ba650eea643f9e406a37862a9cd18399ea38
Matthew Francis <mattfrancis888@gmail.com>
2021-02-16 15:11:44 -0500

    Added NotFound component when user enters invalid url. Changed tab icon and title.

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Body from "./Body";
-
+import NotFound from "./NotFound";
 const Routes = () => {
     return (
         <React.Fragment>
             <Switch>
                 <Route path="/" exact component={Body} />
+                <Route component={NotFound} />
             </Switch>
         </React.Fragment>
     );
